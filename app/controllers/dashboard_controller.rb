530fa7c23e1e4034b9b95ba95686785e499d698a
Lunga Sizani <lsizani@gmail.com>
2015-04-15 17:26:03 +0200

    introduced config file

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,21 @@
 class DashboardController < ApplicationController
+  require 'yaml'
+
   def index
 
+  end
+
+  def settings
+    @settings = YAML::load_file Rails.root.join('config/config.yml')
+  end
+
+  def create
+      my_hash = {params[:setting][:key] => { params[:setting][:val] => params[:setting][:val]}}.to_yaml
+      puts my_hash
+      my_hash = my_hash.gsub('---', '')
+      File.open(Rails.root.join('config/config.yml'), 'a+'){ |f| f.write(my_hash) }
+      @my_file = YAML::load_file Rails.root.join('config/config.yml')
+      redirect_to :controller => 'dashboard', :action => 'settings'
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ InventoryManagerV2::Application.routes.draw do
   get    'login'    => 'sessions#new'
   post   'login'    => 'sessions#create'
   get    'logout'   => 'sessions#destroy'
+  get    'settings' => 'dashboard#settings'
 
   namespace :admin do
     root 'admin#index'
